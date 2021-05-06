b1c78b244f3cc8d571be7bdf6d1be53b78efb6e5
AncuL001 <ancul001@gmail.com>
2021-05-06 21:41:24 +0700

    add function blueprints

--- src/data-structures/linked-list.hpp
+++ src/data-structures/linked-list.hpp
@@ -6,15 +6,55 @@ namespace data_structures {
   class CategoryNode {
     private :
     std::string name;
-    PriorityQueue tasks;
+    TaskPriorityQueue tasks;
+    CategoryNode *prev;
     CategoryNode *next;
+
+    public :
+    /**
+     * @brief Constructor
+     */
+    CategoryNode(std::string name){
+      this->name = name;
+      prev = nullptr;
+      next = nullptr;
+    }
+
+    void changeName(std::string newName){
+      name = newName;
+    }
   };
 
   typedef CategoryNode *CategoryNodePointer;
   
-  class LinkedList {
+  class CategoryList {
     private :
     CategoryNodePointer head;
     CategoryNodePointer tail;
+
+    public :
+    /**
+     * @brief Constructor
+     */
+    CategoryList(){
+      head = nullptr;
+      tail = nullptr;
+    }
+
+    void insertCategory(const CategoryNodePointer newNode){
+      //TODO : Implementasikan
+    }
+
+    void removeCategory(std::string name){
+      //TODO : Implementasikan
+    }
+
+    void changeNodeName(std::string oldName, std::string newName){
+      //TODO : Implementasikan
+    }
+
+    void for_each() const {
+      //TODO : Implementasikan
+    }
   };
 }
--- src/data-structures/queue.hpp
+++ src/data-structures/queue.hpp
@@ -1,19 +1,66 @@
 #pragma once
-#include <string>
+#include <iostream>
+#include <time.h>
 
 namespace data_structures {
   
   class TaskNode {
     private :
     std::string description;
+    tm deadline;
     TaskNode *next;
+
+    public :
+    /**
+     * @brief Constructor
+     */
+    TaskNode(std::string description, tm deadline){
+      this->description = description;
+      this->deadline = deadline;
+      this->next = nullptr;
+    }
+
+    bool isDeadlineEarlier(const TaskNodePointer current) const {
+      //TODO : Implementasikan
+    }
   };
 
   typedef TaskNode *TaskNodePointer;
 
-  class PriorityQueue {
+  class TaskPriorityQueue {
     private :
     TaskNodePointer head;
     TaskNodePointer tail;
+
+    public:
+    /**
+     * @brief Constructor
+     * 
+     * @constructor
+     */
+    TaskPriorityQueue() {
+      head = nullptr;
+      tail = nullptr;
+    }
+
+    bool isEmpty() const {
+      //TODO : Implementasikan
+    }
+
+    void enqueue(const TaskNodePointer newNode){
+      //TODO : Implementasikan 
+    }
+
+    void dequeue(){
+      //TODO : Implementasikan
+    }
+
+    void peek() const {
+      //TODO : Implementasikan
+    }
+
+    void for_each() const {
+      //TODO : Implementasikan
+    }
   };
 }
