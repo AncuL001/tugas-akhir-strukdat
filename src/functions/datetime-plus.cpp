4fbab960223c217b0dc53975763387d72485da6d
AncuL001 <ancul001@gmail.com>
2021-05-14 11:14:51 +0700

    implement functions

--- src/functions/datetime-plus.cpp
+++ src/functions/datetime-plus.cpp
@@ -13,6 +13,19 @@ char compare_deadline(tm lhs, tm rhs){
   return '=';
 }
 
-tm parse_date(std::string string_form){
-  // TODO : Implementasikan
+tm parse_date(const std::string string_form){
+  tm temp;
+  std::stringstream ss(string_form);
+  std::string y, m, d;
+
+  std::getline(ss, y, '-');
+  temp.tm_year = stoi(y);
+
+  std::getline(ss, m, '-');
+  temp.tm_mon  = stoi(m);
+
+  std::getline(ss, d, '-');
+  temp.tm_mday = stoi(d);
+  
+  return temp;
 }
--- src/menus/category-menu.cpp
+++ src/menus/category-menu.cpp
@@ -1,31 +1,76 @@
 #include "category-menu.hpp"
 
 void print_categories(const data_structures::CategoryList list){
-  //TODO : Implementasikan
+  std::cout << "\nDaftar Kategori:";
+  list.for_each([](data_structures::CategoryNodePointer category){
+    std::cout << " " << category->name << "\n";
+  });
 }
 
 void move_category_screen(data_structures::CategoryList &list){
-  //TODO : Implementasikan
+  std::string name;
+  print_categories(list);
+
+  std::cout << "\n0. Batal\n>>";
+  std::getline(std::cin, name);
+  if (name == "0") return;
+
+  list.move_current(name);
 }
 
 void rename_category_screen(data_structures::CategoryList &list){
-  //TODO : Implementasikan
+  std::string oldName, newName;
+  print_categories(list);
+
+  std::cout << "\n0. Batal\n>>";
+  std::getline(std::cin, oldName);
+  if (oldName == "0") return;
+
+  std::cout << "Nama Baru:\n>>";
+  std::getline(std::cin, newName);
+  if (newName == "0") return;
+
+  list.rename_category(oldName, newName);
 }
 
 void add_category_screen(data_structures::CategoryList &list){
-  //TODO : Implementasikan
+  std::string name;
+  std::cout << "0. Batal\n"
+            << "Nama Baru:\n>>";
+  std::getline(std::cin, name);
+  if (name == "0") return;
+
+  list.insert_category(new data_structures::CategoryNode(name));
 }
 
 void remove_category_screen(data_structures::CategoryList &list){
-  //TODO : Implementasikan
+  std::string name;
+  print_categories(list);
+
+  std::cout << "\n0. Batal\n>>";
+  std::getline(std::cin, name);
+  if (name == "0") return;
+
+  list.remove_category(name);
 }
 
 void view_category_screen(const data_structures::CategoryList list){
-  //TODO : Implementasikan
+  print_categories(list);
+  std::cout << "0. Kembali\n>>";
+  char sel;
+  std::cin >> sel;
+  switch (sel){
+    case '0':
+      return;
+
+     default:
+      break;
+  }
 }
 
 void category_options_screen(data_structures::CategoryList &list){
-  std::cout << "Kategori : " << list.current->name << "\n\n"
+  data_structures::CategoryNodePointer current = list.get_current();
+  std::cout << "Kategori : " << current->name << "\n\n"
             << "1. Pindah Kategori\n"
             << "2. Ubah Nama Kategori\n"
             << "3. Tambahkan Kategori\n"
