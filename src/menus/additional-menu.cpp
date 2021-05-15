67943c7e3540c0cc9c12321fc53f8feb8708d5fe
AncuL001 <ancul001@gmail.com>
2021-05-15 22:21:39 +0700

    change output format

--- src/menus/additional-menu.cpp
+++ src/menus/additional-menu.cpp
@@ -20,12 +20,13 @@ void additional_options_screen(data_structures::CategoryList &list){
   // TODO : Implementasikan Stack
   // TODO : Regret my decision to add additional menu
   system("cls");
-  std::cout << "\nAksi Terakhir:\n\n\n\n"
+  std::cout << "\nAksi Terakhir:\n\n"
             << "1. Undo aksi terakhir\n"
             << "2. Impor data\n"
             << "3. Ekspor data\n"
             << "4. Hapus semua data\n"
-            << "0. Kembali";
+            << "0. Kembali\n"
+            << ">> ";
   char sel;
   std::cin >> sel;
   switch (sel){
